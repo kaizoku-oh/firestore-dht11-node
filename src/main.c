b13e71e598850cddced0bbd5d4841a98af5e4abf
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-05-13 03:18:41 +0100

    update component and example

--- src/main.c
+++ src/main.c
@@ -1,16 +1,47 @@
-#include "esp_log.h"
+#include <stdio.h>
+#include <string.h>
+#include <esp_log.h>
+
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+
 #include "dht11.h"
+#include "firestore.h"
+#include "wifi_utils.h"
 
-#define TAG "APP_MAIN"
-#define DHT11_PIN GPIO_NUM_4
+static void _send_data(struct dht11_reading *);
+static void _firestore_task(void *);
 
+#define TAG                                      "APP_MAIN"
+#define DHT11_PIN                                GPIO_NUM_4
+#define DOC_MAX_SIZE                             128
+#define DOC_EXAMPLE                              "{"                          \
+                                                   "\"fields\": {"            \
+                                                     "\"humidity\": {"        \
+                                                       "\"integerValue\": 55" \
+                                                     "},"                     \
+                                                     "\"temperature\": {"     \
+                                                       "\"integerValue\": 23" \
+                                                     "},"                     \
+                                                   "}"                        \
+                                                 "}"
+
+static uint32_t u32DocLength;
+static char tcDoc[DOC_MAX_SIZE];
 static struct dht11_reading stDht11Reading;
 
-void dht11_test(void *pvParameter)
+void app_main(void)
+{
+  wifi_initialise();
+  wifi_wait_connected();
+
+  xTaskCreate(_firestore_task, "firestore", 10240, NULL, 5, NULL);
+}
+
+static void _firestore_task(void *pvParameter)
 {
   DHT11_init(DHT11_PIN);
+  firestore_init();
 
   while(1)
   {
@@ -23,6 +54,7 @@ void dht11_test(void *pvParameter)
                "Temperature: %d °C    Humidity: %d %%",
                stDht11Reading.temperature,
                stDht11Reading.humidity);
+      _send_data(&stDht11Reading);
     } 
     else
     {
@@ -36,7 +68,34 @@ void dht11_test(void *pvParameter)
   }
 }
 
-void app_main(void)
+static void _send_data(struct dht11_reading *pstReading)
 {
-  xTaskCreate(dht11_test, "dht11_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+  /* Format document with newly fetched data */
+  u32DocLength = snprintf(tcDoc,
+                          sizeof(tcDoc),
+                          "{\"fields\":{\"humidity\":{\"integerValue\":%d},\"temperature\":{\"integerValue\":%d}}}",
+                          pstReading->humidity,
+                          pstReading->temperature);
+  ESP_LOGD(TAG, "Document length after formatting: %d", u32DocLength);
+  if(u32DocLength > 0)
+  {
+    /* Update document in firestore or create it if it doesn't already exists */
+    if(FIRESTORE_OK == firestore_update_document("devices",
+                                                 "dht11-node",
+                                                 tcDoc,
+                                                 &u32DocLength))
+    {
+      ESP_LOGI(TAG, "Document updated successfully");
+      ESP_LOGD(TAG, "Updated document length: %d", u32DocLength);
+      ESP_LOGD(TAG, "Updated document content:\r\n%.*s", u32DocLength, tcDoc);
+    }
+    else
+    {
+      ESP_LOGE(TAG, "Couldn't update document");
+    }
+  }
+  else
+  {
+    ESP_LOGE(TAG, "Couldn't format document");
+  }
 }
