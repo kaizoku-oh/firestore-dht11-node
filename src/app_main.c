a09c472c2f623ef19553f813062fcbe91893c808
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-05-14 17:41:47 +0100

    Add app_time

--- src/app_main.c
+++ src/app_main.c
@@ -8,7 +8,9 @@
 
 #include "dht11.h"
 #include "firestore.h"
+
 #include "app_wifi.h"
+#include "app_time.h"
 #include "app_ota.h"
 
 static void _app_main_send_data(struct dht11_reading *);
@@ -20,16 +22,19 @@ static void _app_main_firestore_task(void *);
 #define APP_MAIN_FIRESTORE_TASK_STACK_SIZE       10240
 #define APP_MAIN_FIRESTORE_TASK_PRIORITY         4
 #define APP_MAIN_FIRESTORE_PERIOD_MS             2500
-#define APP_MAIN_FIRESTORE_DOC_MAX_SIZE          128
-#define APP_MAIN_FIRESTORE_DOC_EXAMPLE           "{"                          \
-                                                   "\"fields\": {"            \
-                                                     "\"humidity\": {"        \
-                                                       "\"integerValue\": 55" \
-                                                     "},"                     \
-                                                     "\"temperature\": {"     \
-                                                       "\"integerValue\": 23" \
-                                                     "},"                     \
-                                                   "}"                        \
+#define APP_MAIN_FIRESTORE_DOC_MAX_SIZE          160
+#define APP_MAIN_FIRESTORE_DOC_EXAMPLE           "{"                                     \
+                                                   "\"fields\": {"                       \
+                                                     "\"humidity\": {"                   \
+                                                       "\"integerValue\": 55"            \
+                                                     "},"                                \
+                                                     "\"temperature\": {"                \
+                                                       "\"integerValue\": 23"            \
+                                                     "},"                                \
+                                                     "\"timestamp\": {"                  \
+                                                       "\"integerValue\": 1621010203262" \
+                                                     "},"                                \
+                                                   "}"                                   \
                                                  "}"
 
 static uint32_t u32DocLength;
@@ -40,7 +45,10 @@ void app_main(void)
 {
   app_wifi_init();
   app_wifi_wait();
+
   app_ota_start();
+
+  app_time_init();
 
   xTaskCreate(_app_main_firestore_task,
               "firestore",
@@ -82,12 +90,28 @@ static void _app_main_firestore_task(void *pvParameter)
 
 static void _app_main_send_data(struct dht11_reading *pstReading)
 {
+  int64_t s64Timestamp;
+
   /* Format document with newly fetched data */
-  u32DocLength = snprintf(tcDoc,
-                          sizeof(tcDoc),
-                          "{\"fields\":{\"humidity\":{\"integerValue\":%d},\"temperature\":{\"integerValue\":%d}}}",
-                          pstReading->humidity,
-                          pstReading->temperature);
+  if(ESP_OK == app_time_get_ts(&s64Timestamp))
+  {
+    ESP_LOGI(APP_MAIN_TAG, "Current timestamp: %lld", s64Timestamp);
+    u32DocLength = snprintf(tcDoc,
+                            sizeof(tcDoc),
+                            "{\"fields\":{\"humidity\":{\"integerValue\":%d},\"temperature\":{\"integerValue\":%d},\"timestamp\":{\"integerValue\":%lld}}}",
+                            pstReading->humidity,
+                            pstReading->temperature,
+                            s64Timestamp);
+  }
+  else
+  {
+    ESP_LOGW(APP_MAIN_TAG, "Failed to get timestamp --> formatting data without timestamp");
+    u32DocLength = snprintf(tcDoc,
+                            sizeof(tcDoc),
+                            "{\"fields\":{\"humidity\":{\"integerValue\":%d},\"temperature\":{\"integerValue\":%d}}}",
+                            pstReading->humidity,
+                            pstReading->temperature);
+  }
   ESP_LOGD(APP_MAIN_TAG, "Document length after formatting: %d", u32DocLength);
   if(u32DocLength > 0)
   {
